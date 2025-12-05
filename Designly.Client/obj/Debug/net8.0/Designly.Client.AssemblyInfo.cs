e66ca3345bc93be94b869f42ca7002ae467266ae
EnriqueCedeno <e.cedeno31@gmail.com>
2025-12-05 10:58:17 -0400

    Remove `run-all.ps1` script and update `.gitignore` to include IDE-specific files

--- Designly.Client/obj/Debug/net8.0/Designly.Client.AssemblyInfo.cs
+++ Designly.Client/obj/Debug/net8.0/Designly.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Designly.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe3a52c7162af1c24202e50602cbcd9462dda04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Designly.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Designly.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
